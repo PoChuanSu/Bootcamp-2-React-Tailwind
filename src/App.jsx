b1105f054c5b0480de44f9874de09c846419533c
PoChuanSu <peterpeter1010@live.com>
2025-09-15 20:30:18 +1000

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <>
-            <h1>Hello Tailwind</h1>
+            <h1 className="text-3xl font-bold underline">Hello Tailwind</h1>
         </>
     );
 }
